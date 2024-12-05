23fd472b0abfc42b11c5cf1485e27e7ad6f7f27c
Clupai8o0 <clupaio4@gmail.com>
2024-12-05 14:56:24 +1100

    bold fonting for search bar

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -28,7 +28,7 @@ const Search = React.forwardRef<
 			<input
 				type={type}
 				className={cn(
-					"flex h-9 w-full rounded-md border-none bg-transparent mr-2 px-3 text-base transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+					"flex h-9 w-full rounded-md border-none bg-transparent mr-2 px-3 text-base transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm font-bold",
 					className
 				)}
 				ref={ref}
